84c3010043d62d08966c85a7eb91b0d45324f8a1
samin-z <55046964+samin-z@users.noreply.github.com>
2023-05-04 17:31:03 +0200

    Update FizzBuzz.php

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -2,20 +2,19 @@
 class FizzBuzz {
     public function fizzBuzzSequence (): void
     {
-        for ($i = 1; $i <= 100; $i++) {
-          $fizzNumber = ($i % 3 === 0);
-          $buzzNumber = ($i % 5 === 0);
-
-          if($fizzNumber && $buzzNumber) {
-            echo "FizzBuzz,\n";
-          } elseif($fizzNumber) {
-            echo "Fizz,\n";
-          } elseif($buzzNumber) {
-            echo "Buzz,\n";
-          } else {
-            echo $i.",\n";
-          }
+      $fizzBuzzStack = [];
+  
+      for ($i = 1; $i <= 100; $i++) {
+      $thisLoop = [];
+        if($i % 3 === 0){
+          $thisLoop[] = 'Fizz';
         }
+        if($i % 5 === 0){
+          $thisLoop[] = 'Buzz';
+        }
+        $fizzBuzzStack[] = empty($thisLoop) ? $i : implode('',$thisLoop);
+      }
+      echo implode(',',$fizzBuzzStack);
     }
 }
 
